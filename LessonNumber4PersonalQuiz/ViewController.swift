3ac1a976a06d70e74892a5754703ec73cddc6f9e
Vladimir <kynicin87@mail.ru>
2021-01-13 16:45:44 +0200

    Initial Commit

---
+++ LessonNumber4PersonalQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LessonNumber4PersonalQuiz
+//
+//  Created by vladimir gennadievich on 13.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
