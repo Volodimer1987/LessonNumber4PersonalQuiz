d4e08b16ae84a8483a643953b69089f0dda5b00a
Vladimir <kynicin87@mail.ru>
2021-01-14 04:26:33 +0200

    First Part is ready

---
+++ LessonNumber4PersonalQuiz/Model/Answer.swift
@@ -0,0 +1,13 @@
+//
+//  Answer.swift
+//  LessonNumber4PersonalQuiz
+//
+//  Created by vladimir gennadievich on 14.01.2021.
+//
+
+import Foundation
+
+struct Answer {
+    var text:String
+    var type:AnimalType
+}
---
+++ LessonNumber4PersonalQuiz/Model/ResponseType.swift
@@ -0,0 +1,14 @@
+//
+//  ResponseType.swift
+//  LessonNumber4PersonalQuiz
+//
+//  Created by vladimir gennadievich on 14.01.2021.
+//
+
+import Foundation
+
+enum ResponseType {
+    case single
+    case multiple
+    case ranged
+}
